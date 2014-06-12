ea8d571bc78f4a23d72821a710a17288ed2f1bd1
Jeremy Dyer <jdye64@gmail.com>
2014-06-12 11:47:12 -0400

    AZ changes

--- engagetest/src/main/java/com/silverpop/engage/demo/engagetest/fragment/NotificationsFragment.java
+++ engagetest/src/main/java/com/silverpop/engage/demo/engagetest/fragment/NotificationsFragment.java
@@ -13,7 +13,6 @@ import android.view.ViewGroup;
 import android.widget.Button;
 
 import com.silverpop.engage.UBFManager;
-import com.silverpop.engage.EngageApplication;
 import com.silverpop.engage.demo.engagetest.EngageNotificationReceiver;
 import com.silverpop.engage.demo.engagetest.R;
 
