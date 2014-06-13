03e6e54ad17a10d3ac31328751f1902ed9284f51
bahzuercher <az@ehirelabs.com>
2014-06-12 22:53:29 -0400

    integrated urban airship push in the test app

---
+++ engagetest/src/main/java/com/silverpop/engage/demo/engagetest/Application.java
@@ -0,0 +1,26 @@
+package com.silverpop.engage.demo.engagetest;
+
+import android.util.Log;
+
+import com.silverpop.engage.EngageApplication;
+import com.urbanairship.AirshipConfigOptions;
+import com.urbanairship.UAirship;
+import com.urbanairship.push.PushManager;
+
+/**
+ * Created by azuercher on 6/12/14.
+ */
+public class Application extends EngageApplication {
+    @Override
+    public void onCreate(){
+        UAirship.takeOff(this);
+        PushManager.enablePush();
+        String apid = PushManager.shared().getAPID();
+        Log.e("engagetest", "My Application onCreate - App APID: " + apid);
+        PushManager.shared().setIntentReceiver(PushReceiver.class);
+
+        super.onCreate();
+
+    }
+
+}
