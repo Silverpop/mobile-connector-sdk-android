b9c25d419e67471bc3c40459c458e6fc5efc3598
Jeremy Dyer <jdye64@gmail.com>
2014-09-05 15:35:03 -0400

    Updated the UBFClient to help prevent concurrent DB UBFEvents. There existed a small chance before that if the manual "postUBFCache" method was called by the application consuming the SDK that the list of unposted events could contain an event from the database query that was actually already making an attempt to be sent the Silverpop leading to multiple copies of the event being sent to Silverpop.

--- engage/src/main/java/com/silverpop/engage/network/UBFClient.java
+++ engage/src/main/java/com/silverpop/engage/network/UBFClient.java
@@ -18,6 +18,7 @@ import org.json.JSONObject;
 
 import java.net.URI;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -33,11 +34,15 @@ public class UBFClient
     private EngageLocalEventStore engageLocalEventStore = null;
     private int maxRetries = 3;
 
+    //List of inprogress events kept in memory. Helps with concurrency issues and preventing events from being sent twice.
+    private ArrayList<Long> inProgressEventIds = null;
+
     private UBFClient(Context context, String clientId, String secret, String refreshToken, String host) {
         super(context, clientId, secret, refreshToken, host);
 
         maxRetries = EngageConfigManager.get(context).maxNumRetries();
         engageLocalEventStore = EngageLocalEventStore.get(context);
+        inProgressEventIds = new ArrayList<Long>();
 
         try {
             if (!engageLocalEventStore.isConnected()) {
@@ -95,58 +100,76 @@ public class UBFClient
             final EngageEvent[] unpostedEvents = engageLocalEventStore.findUnpostedEvents();
 
             if (unpostedEvents != null && unpostedEvents.length > 0) {
+
                 //Create the JSONObject wrapped around the "events" tag engage expects.
                 JSONObject events = new JSONObject();
+                final ArrayList<EngageEvent> stackCopyOfIds = new ArrayList<EngageEvent>();
 
                 try {
                     JSONArray eventsArray = new JSONArray();
+
+                    //Adds the events to the JSON array and makes sure that the events are not already in progress.
                     for (EngageEvent ee : unpostedEvents) {
-                        eventsArray.put(new JSONObject(ee.getEventJson()));
-                    }
-                    events.put("events", eventsArray);
-
-                    final JsonObjectRequest jsonRequest = new JsonObjectRequest(Request.Method.POST,
-                            getUBFURL(), events,
-                            new Response.Listener<JSONObject>() {
-                                public void onResponse(JSONObject response) {
-                                    //TODO: Add extra logic to make sure that the message was successful based on response payload
-                                    //Update all of the EngageEvents to SUCCESSFULLY_POSTED
-                                    for (EngageEvent ee : unpostedEvents) {
-                                        ee.setEventStatus(EngageEvent.SUCCESSFULLY_POSTED);
-                                        engageLocalEventStore.saveUBFEvent(ee);
-                                    }
-                                }
-                            },
-                            new Response.ErrorListener() {
-                                @Override
-                                public void onErrorResponse(VolleyError error) {
-                                    //Update the failure count and then fail the status if the maximum has been reached.
-                                    for (EngageEvent ee : unpostedEvents) {
-                                        ee.setEventFailedPostCount(ee.getEventFailedPostCount() + 1);
-                                        if (ee.getEventFailedPostCount() >= maxRetries) {
-                                            ee.setEventStatus(EngageEvent.FAILED_POST);
-                                        }
-                                        engageLocalEventStore.saveUBFEvent(ee);
-                                    }
-                                }
-                            }
-                    )
-                    {
-                        @Override
-                        public Map<String, String> getHeaders() throws AuthFailureError {
-                            Map<String, String> params = new HashMap<String, String>();
-                            String oauthToken = getOauthToken();
-                            if (oauthToken != null) {
-                                params.put("Authorization", "Bearer " + getOauthToken());
-                            } else {
-                                Log.w(TAG, "Client authentication has expired. Event post will fail " +
-                                        "but will be reattempted after new authentication is complete.");
-                            }
-
-                            return params;
+                        if (!inProgressEventIds.contains(ee.getId())) {
+                            eventsArray.put(new JSONObject(ee.getEventJson()));
+                            inProgressEventIds.add(new Long(ee.getId()));
+                            stackCopyOfIds.add(ee);
                         }
-                    };
-                    request(jsonRequest);
+                    }
+
+                    //Only post if the events array contains elements.
+                    if (eventsArray.length() > 0) {
+                        events.put("events", eventsArray);
+
+                        final JsonObjectRequest jsonRequest = new JsonObjectRequest(Request.Method.POST,
+                                getUBFURL(), events,
+                                new Response.Listener<JSONObject>() {
+                                    public void onResponse(JSONObject response) {
+                                        //TODO: Add extra logic to make sure that the message was successful based on response payload
+                                        //Update all of the EngageEvents to SUCCESSFULLY_POSTED
+                                        for (EngageEvent ee : stackCopyOfIds) {
+                                            ee.setEventStatus(EngageEvent.SUCCESSFULLY_POSTED);
+                                            engageLocalEventStore.saveUBFEvent(ee);
+
+                                            //Removes the EngageEvent from the list of "inProgress" events.
+                                            if (inProgressEventIds.contains(ee.getId())) {
+                                                inProgressEventIds.remove(ee.getId());
+                                            }
+                                        }
+                                    }
+                                },
+                                new Response.ErrorListener() {
+                                    @Override
+                                    public void onErrorResponse(VolleyError error) {
+                                        //Update the failure count and then fail the status if the maximum has been reached.
+                                        for (EngageEvent ee : stackCopyOfIds) {
+                                            ee.setEventFailedPostCount(ee.getEventFailedPostCount() + 1);
+                                            if (ee.getEventFailedPostCount() >= maxRetries) {
+                                                ee.setEventStatus(EngageEvent.FAILED_POST);
+                                            }
+                                            inProgressEventIds.remove(ee.getId());
+                                            engageLocalEventStore.saveUBFEvent(ee);
+                                        }
+                                    }
+                                }
+                        )
+                        {
+                            @Override
+                            public Map<String, String> getHeaders() throws AuthFailureError {
+                                Map<String, String> params = new HashMap<String, String>();
+                                String oauthToken = getOauthToken();
+                                if (oauthToken != null) {
+                                    params.put("Authorization", "Bearer " + getOauthToken());
+                                } else {
+                                    Log.w(TAG, "Client authentication has expired. Event post will fail " +
+                                            "but will be reattempted after new authentication is complete.");
+                                }
+
+                                return params;
+                            }
+                        };
+                        request(jsonRequest);
+                    }
 
                 } catch (JSONException e) {
                     e.printStackTrace();
