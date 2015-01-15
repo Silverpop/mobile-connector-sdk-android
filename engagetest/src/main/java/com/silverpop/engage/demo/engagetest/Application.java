60dc3c51458f6c572c9a313290eb844dbf5a2c71
Lindsay Thurmond <lindsay.thurmond@makeandbuild.com>
2015-01-14 20:32:08 -0500

    add setup recipient user button to engagetest application

--- engagetest/src/main/java/com/silverpop/engage/demo/engagetest/Application.java
+++ engagetest/src/main/java/com/silverpop/engage/demo/engagetest/Application.java
@@ -1,9 +1,7 @@
 package com.silverpop.engage.demo.engagetest;
 
 import android.util.Log;
-
 import com.silverpop.engage.EngageApplication;
-import com.silverpop.engage.config.EngageConfig;
 import com.urbanairship.UAirship;
 import com.urbanairship.push.PushManager;
 
@@ -11,17 +9,33 @@ import com.urbanairship.push.PushManager;
  * Created by azuercher on 6/12/14.
  */
 public class Application extends EngageApplication {
+
+    public static final String ENGAGE_TEST_TAG = "engagetest";
+
     @Override
     public void onCreate(){
         UAirship.takeOff(this);
         PushManager.enablePush();
         String apid = PushManager.shared().getAPID();
-        Log.e("engagetest", "My Application onCreate - App APID: " + apid);
+        Log.e(ENGAGE_TEST_TAG, "My Application onCreate - App APID: " + apid);
         PushManager.shared().setIntentReceiver(PushReceiver.class);
 
         super.onCreate();
 
-        EngageConfig.storeMobileUserId(getApplicationContext(), "secret@test.com");
+//        EngageConfig.storeMobileUserId(getApplicationContext(), "secret@test.com");
+
+
+//        MobileConnectorManager.get().setupRecipient(new SetupRecipientHandler() {
+//            @Override
+//            public void onSuccess(SetupRecipientResult result) {
+//                Log.i(ENGAGE_TEST_TAG, "Setup recipient auto configured recipient id: " + result.getRecipientId());
+//            }
+//
+//            @Override
+//            public void onFailure(Exception exception) {
+//                Log.e(ENGAGE_TEST_TAG, "Failed to setup recipient: " + exception.getMessage(), exception);
+//            }
+//        });
     }
 
 }
--- engagetest/src/main/java/com/silverpop/engage/demo/engagetest/fragment/XMLAPIFragment.java
+++ engagetest/src/main/java/com/silverpop/engage/demo/engagetest/fragment/XMLAPIFragment.java
@@ -11,15 +11,21 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import com.android.volley.VolleyError;
+import com.silverpop.engage.AnonymousMobileConnectorManager;
+import com.silverpop.engage.MobileConnectorManager;
 import com.silverpop.engage.XMLAPIManager;
 import com.silverpop.engage.config.EngageConfig;
 import com.silverpop.engage.config.EngageConfigManager;
 import com.silverpop.engage.demo.engagetest.R;
 import com.silverpop.engage.domain.XMLAPI;
+import com.silverpop.engage.exception.XMLAPIResponseException;
 import com.silverpop.engage.exception.XMLResponseParseException;
+import com.silverpop.engage.recipient.SetupRecipientHandler;
+import com.silverpop.engage.recipient.SetupRecipientResult;
+import com.silverpop.engage.response.AddRecipientResponse;
 import com.silverpop.engage.response.EngageResponseXML;
+import com.silverpop.engage.response.handler.AddRecipientResponseHandler;
 
 /**
  * Created by jeremydyer on 6/5/14.
@@ -32,6 +38,7 @@ public class XMLAPIFragment
     private Button mCreateAnonymousUserButton;
     private Button mUpgradeAnonymousUserButton;
     private Button mMergeAnonymousUserButton;
+    private Button setupRecipientUserButton;
 
     private XMLAPIManager xmlapiManager = null;
     private TextView xmlApiResultTextView = null;
@@ -46,114 +53,49 @@ public class XMLAPIFragment
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
-        View v = inflater.inflate(R.layout.xmlapi_view, container, false);
+        View view = inflater.inflate(R.layout.xmlapi_view, container, false);
 
         final Resources r = getActivity().getResources();
 
         xmlapiManager = XMLAPIManager.get();
 
-        xmlApiResultTextView = (TextView)v.findViewById(R.id.xmlApiResultTextView);
-        mCreateAnonymousUserButton = (Button)v.findViewById(R.id.createAnonymousUserButton);
-        mCreateAnonymousUserButton.setOnClickListener(new View.OnClickListener() {
+        xmlApiResultTextView = (TextView)view.findViewById(R.id.xmlApiResultTextView);
+
+        createAnonymousUserButton(view);
+        createUpgradeAnonymousUserButton(view);
+        createMergeAnonymousUserButton(view);
+        setupRecipientUserButton(view);
+
+        return view;
+    }
+
+    private void setupRecipientUserButton(View view) {
+        setupRecipientUserButton = (Button) view.findViewById(R.id.setupRecipientButton);
+        setupRecipientUserButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                xmlapiManager.createAnonymousUserList(EngageConfigManager.get(getActivity()).engageListId(), new AsyncTask<EngageResponseXML, Void, Object>() {
+            public void onClick(View v) {
+
+                MobileConnectorManager.get().setupRecipient(new SetupRecipientHandler() {
                     @Override
-                    protected EngageResponseXML doInBackground(EngageResponseXML... engageResponseXMLs) {
-                        return engageResponseXMLs[0];
+                    public void onSuccess(SetupRecipientResult result) {
+                        String message = "Setup recipient auto configured recipient id: " + result.getRecipientId();
+                        Log.i(TAG, message);
+                        showToast(message);
                     }
 
                     @Override
-                    protected void onPostExecute(Object responseObject) {
-                        try {
-                            EngageResponseXML responseXML = (EngageResponseXML)responseObject;
-                            String result = responseXML.valueForKeyPath("envelope.body.result.success");
-                            if (result.equalsIgnoreCase("true")) {
-                                String id = responseXML.valueForKeyPath("envelope.body.result.recipientid");
-                                recipientId = id;
-                                EngageConfig.storeAnonymousUserId(getActivity(), recipientId);
-                                xmlApiResultTextView.setText("Anonymous User RecipientID : " + id);
-                                mUpgradeAnonymousUserButton.setEnabled(true);
-                                mMergeAnonymousUserButton.setEnabled(true);
-                            } else {
-                                String faultString = responseXML.valueForKeyPath("envelope.body.fault.faultstring");
-                                xmlApiResultTextView.setText("ERROR: " + faultString);
-                            }
-
-                            Toast.makeText(getActivity().getApplicationContext(), result, Toast.LENGTH_SHORT).show();
-                        } catch (XMLResponseParseException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                }, new AsyncTask<VolleyError, Void, Object>() {
-                    @Override
-                    protected Object doInBackground(VolleyError... volleyErrors) {
-                        Log.e(TAG, "Failure is posting create anonymous user event to silverpop");
-                        return volleyErrors[0];
-                    }
-
-                    @Override
-                    protected void onPostExecute(Object responseObject) {
-                        VolleyError error = (VolleyError)responseObject;
-                        Toast.makeText(getActivity().getApplicationContext(), "Error creating anonymous user: "
-                                + error.getMessage(), Toast.LENGTH_SHORT).show();
+                    public void onFailure(Exception exception) {
+                        String message = "Failed to setup recipient";
+                        Log.e(TAG, message + ": " + exception.getMessage(), exception);
+                        showToast(message);
                     }
                 });
             }
         });
+    }
 
-        mUpgradeAnonymousUserButton = (Button)v.findViewById(R.id.upgradeAnonymousUserButton);
-        mUpgradeAnonymousUserButton.setEnabled(false);
-        mUpgradeAnonymousUserButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                XMLAPI addRecipient = XMLAPI.addRecipientWithEmail(EngageConfig.mobileUserId(getActivity()), EngageConfigManager.get(getActivity()).engageListId());
-                xmlapiManager.postXMLAPI(addRecipient, new AsyncTask<EngageResponseXML, Void, Object>() {
-
-                    @Override
-                    protected EngageResponseXML doInBackground(EngageResponseXML... engageResponseXMLs) {
-                        return engageResponseXMLs[0];
-                    }
-
-                    @Override
-                    protected void onPostExecute(Object responseObject) {
-                        try {
-                            EngageResponseXML responseXML = (EngageResponseXML)responseObject;
-                            String result = responseXML.valueForKeyPath("envelope.body.result.success");
-                            if (result.equalsIgnoreCase("true")) {
-                                Toast.makeText(getActivity().getApplicationContext(), responseXML.getXml(), Toast.LENGTH_LONG).show();
-                                String id = responseXML.valueForKeyPath("envelope.body.result.recipientid");
-                                recipientId = id;
-                                xmlApiResultTextView.setText("Anonymous User RecipientID : " + id);
-                            } else {
-                                String faultString = responseXML.valueForKeyPath("envelope.body.fault.faultstring");
-                                xmlApiResultTextView.setText("ERROR: " + faultString);
-                            }
-
-                            Toast.makeText(getActivity().getApplicationContext(), result, Toast.LENGTH_SHORT).show();
-                        } catch (XMLResponseParseException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                }, new AsyncTask<VolleyError, Void, Object>() {
-                    @Override
-                    protected Object doInBackground(VolleyError... volleyErrors) {
-                        Log.e(TAG, "Failure is posting create anonymous user event to silverpop");
-                        return volleyErrors[0];
-                    }
-
-                    @Override
-                    protected void onPostExecute(Object responseObject) {
-                        VolleyError error = (VolleyError)responseObject;
-                        Toast.makeText(getActivity().getApplicationContext(), "Error creating anonymous user: "
-                                + error.getMessage(), Toast.LENGTH_SHORT).show();
-                    }
-                });
-            }
-        });
-
-
-        mMergeAnonymousUserButton = (Button)v.findViewById(R.id.mergeAnonymousUserButton);
+    private void createMergeAnonymousUserButton(View view) {
+        mMergeAnonymousUserButton = (Button)view.findViewById(R.id.mergeAnonymousUserButton);
         mMergeAnonymousUserButton.setEnabled(false);
         mMergeAnonymousUserButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -172,7 +114,7 @@ public class XMLAPIFragment
                             EngageResponseXML responseXML = (EngageResponseXML)responseObject;
                             String result = responseXML.valueForKeyPath("envelope.body.result.success");
                             if (result.equalsIgnoreCase("true")) {
-                                Toast.makeText(getActivity().getApplicationContext(), responseXML.getXml(), Toast.LENGTH_LONG).show();
+                                showToast(responseXML.getXml());
                                 String id = responseXML.valueForKeyPath("envelope.body.result.recipientid");
                                 recipientId = id;
                                 xmlApiResultTextView.setText("Anonymous User Merged to known user : " + id);
@@ -181,7 +123,7 @@ public class XMLAPIFragment
                                 xmlApiResultTextView.setText("ERROR: " + faultString);
                             }
 
-                            Toast.makeText(getActivity().getApplicationContext(), result, Toast.LENGTH_SHORT).show();
+                            showToast(result);
                         } catch (XMLResponseParseException e) {
                             e.printStackTrace();
                         }
@@ -196,13 +138,99 @@ public class XMLAPIFragment
                     @Override
                     protected void onPostExecute(Object responseObject) {
                         VolleyError error = (VolleyError)responseObject;
-                        Toast.makeText(getActivity().getApplicationContext(), "Error creating anonymous user: "
-                                + error.getMessage(), Toast.LENGTH_SHORT).show();
+                        showToast("Error creating anonymous user: "
+                                + error.getMessage());
                     }
                 });
             }
         });
-
-        return v;
     }
+
+    private void createUpgradeAnonymousUserButton(View view) {
+        mUpgradeAnonymousUserButton = (Button) view.findViewById(R.id.upgradeAnonymousUserButton);
+        mUpgradeAnonymousUserButton.setEnabled(false);
+        mUpgradeAnonymousUserButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final XMLAPI addRecipient = XMLAPI.addRecipientWithEmail(EngageConfig.mobileUserId(getActivity()), EngageConfigManager.get(getActivity()).engageListId());
+                xmlapiManager.postXMLAPI(addRecipient, new AddRecipientResponseHandler() {
+                    @Override
+                    public void onAddRecipientSuccess(AddRecipientResponse addRecipientResponse) {
+                        Toast.makeText(getActivity().getApplicationContext(), addRecipientResponse.getResponseXml().getXml(), Toast.LENGTH_LONG).show();
+                        String id = addRecipientResponse.getRecipientId();
+                        recipientId = id;
+                        xmlApiResultTextView.setText("Anonymous User RecipientID : " + id);
+                        showToast("SUCCESS: Recipient id = " + recipientId);
+                    }
+
+                    @Override
+                    public void onFailure(Exception error) {
+                        if (error instanceof XMLAPIResponseException) {
+                            String faultString = ((XMLAPIResponseException) error).getXmlResponse().getFaultString();
+                            xmlApiResultTextView.setText("ERROR: " + faultString);
+                            showToast("ERROR");
+                        } else {
+                            showToast("Error creating anonymous user: " + error.getMessage());
+                        }
+                    }
+                });
+            }
+        });
+    }
+
+    private void createAnonymousUserButton(View view) {
+        mCreateAnonymousUserButton = (Button)view.findViewById(R.id.createAnonymousUserButton);
+        mCreateAnonymousUserButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AnonymousMobileConnectorManager.get().createAnonymousUserList(EngageConfigManager.get(getActivity()).engageListId(), new AsyncTask<EngageResponseXML, Void, Object>() {
+                    @Override
+                    protected EngageResponseXML doInBackground(EngageResponseXML... engageResponseXMLs) {
+                        return engageResponseXMLs[0];
+                    }
+
+                    @Override
+                    protected void onPostExecute(Object responseObject) {
+                        try {
+                            EngageResponseXML responseXML = (EngageResponseXML) responseObject;
+                            String result = responseXML.valueForKeyPath("envelope.body.result.success");
+                            if (result.equalsIgnoreCase("true")) {
+                                String id = responseXML.valueForKeyPath("envelope.body.result.recipientid");
+                                recipientId = id;
+                                EngageConfig.storeAnonymousUserId(getActivity(), recipientId);
+                                xmlApiResultTextView.setText("Anonymous User RecipientID : " + id);
+                                mUpgradeAnonymousUserButton.setEnabled(true);
+                                mMergeAnonymousUserButton.setEnabled(true);
+                            } else {
+                                String faultString = responseXML.valueForKeyPath("envelope.body.fault.faultstring");
+                                xmlApiResultTextView.setText("ERROR: " + faultString);
+                            }
+
+                            showToast(result);
+                        } catch (XMLResponseParseException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new AsyncTask<VolleyError, Void, Object>() {
+                    @Override
+                    protected Object doInBackground(VolleyError... volleyErrors) {
+                        Log.e(TAG, "Failure is posting create anonymous user event to silverpop");
+                        return volleyErrors[0];
+                    }
+
+                    @Override
+                    protected void onPostExecute(Object responseObject) {
+                        VolleyError error = (VolleyError) responseObject;
+                        showToast("Error creating anonymous user: "
+                                + error.getMessage());
+                    }
+                });
+            }
+        });
+    }
+
+    private void showToast(String text) {
+        Toast.makeText(getActivity().getApplicationContext(), text, Toast.LENGTH_SHORT).show();
+    }
+
 }
